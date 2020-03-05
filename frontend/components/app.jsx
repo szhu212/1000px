caefc4b83db14d1007532700f135e4d2f10d8db3
szhu212 <skylarzhu212@gmail.com>
2020-03-04 17:40:54 -0800

    Recreate app.jsx

---
+++ frontend/components/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavBarContainer from'./nav_bar/nav_bar_container';
+import LoginFormContainer from './auth/login_form_container';
+import SignupFormContainer from './auth/signup_form_container';
+import { Route, Switch} from "react-router-dom";
+import { AuthRoute, Protected} from '../util/route_util'
+
+const App = () => (
+    <div>
+        <header>
+        <NavBarContainer />
+        </header>
+    <Switch >
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+
+    </Switch>
+
+    </div>
+)
+
+export default App;
